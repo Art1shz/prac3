985421c68a72661345c2e275400ee0832f7157ca
Art1shz <art1shz@mail.ru>
2023-03-10 03:47:49 +0300

    prac3

---
+++ на 3.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml;
+
+Console.WriteLine("Выберите способ решения: \r\nФормулы - 1 \r\nЦиклы - 2");
+int sposob = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\r\nВыберите прогрессию: \r\nГеометрическая - 1 \r\nАрифметическая - 2");
+int prog = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\r\nВведите первый член");
+double a1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\r\nВведите знаменатель/разность  прогрессии");
+double qd = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\r\nВведите номер последнего члена");
+double n = Convert.ToDouble(Console.ReadLine());
+
+if (sposob == 1 & prog == 1)
+{
+    double otvet11 = a1 * ((Math.Pow(qd, n)) - 1) / (qd - 1);
+    Console.WriteLine($"Сумма геометрической прогрессии равна: {otvet11}");
+}
+if (sposob == 1 & prog == 2)
+{
+    double otvet12 = (a1 + (a1 + qd * (n - 1))) * n / 2;
+    Console.WriteLine($"Сумма арифметической прогрессии равна: {otvet12}");
+}
+if (sposob == 2 & prog == 1)
+{
+    double otvet21 = a1;
+    for (double i = 0; i < (n - 1); i++)
+    {
+        a1 = a1 * qd;
+        otvet21 = otvet21 + a1;
+    }
+    Console.WriteLine($"Сумма геометрической прогрессии равна:");
+    Console.WriteLine(otvet21);
+}
+if (sposob == 2 & prog == 2)
+{
+    double s = a1;
+    double otvet22 = a1;
+    for (double i = 0; i < (n - 1); i++)
+    {
+        s = s + qd;
+        otvet22 = otvet22 + s;
+    }
+    Console.WriteLine($"Сумма арифметической прогрессии равна:");
+    Console.WriteLine(otvet22);
+}
---
+++ на 5 (без цикла).cs
@@ -0,0 +1,27 @@
+﻿int[] mas = new int[7]
+{ 5000, 1000, 500, 200, 100, 10, 1};
+
+Console.WriteLine("Введите сумму,которую необходимо представить в количестве купюр n номинала.");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n - (n / mas[0]) * mas[0];
+int b = n - (n / mas[1]) * mas[1];
+int c = n - (n / mas[2]) * mas[2];
+int d = n - (n / mas[3]) * mas[3];
+int e = n - (n / mas[4]) * mas[4];
+int f = n - (n / mas[5]) * mas[5];
+
+if (n >= mas[0])
+    Console.WriteLine($"{n / mas[0]} Купюры номиналом {mas[0]}");
+if (a >= mas[1])
+    Console.WriteLine($"{a / mas[1]} Купюры номиналом {mas[1]}");
+if (b >= mas[2])
+    Console.WriteLine($"{b / mas[2]} Купюры номиналом {mas[2]}");
+if (c >= mas[3])
+    Console.WriteLine($"{c / mas[3]} Купюры номиналом {mas[3]}");
+if (d >= mas[4])
+    Console.WriteLine($"{d / mas[4]} Купюры номиналом {mas[4]}");
+if (e >= mas[5])
+    Console.WriteLine($"{e / mas[5]} Купюры номиналом {mas[5]}");
+if (f >= mas[6])
+    Console.WriteLine($"{f / mas[6]} Купюры номиналом {mas[6]}");
